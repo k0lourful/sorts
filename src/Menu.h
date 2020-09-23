770c0cd327a40e7e1417071c1359133eb992a9a9
Daniel White <xt1zer@outlook.com>
2020-09-23 15:33:01 +0300

    Add menu interaction

--- src/Menu.h
+++ src/Menu.h
@@ -1,166 +1,105 @@
 #pragma once
+
 #include <iostream>
-#include "listStreams.h"
+#include "DynArray.h"
 #include "clearScreen.h"
 
-void choice(const int &c, ListStreams &list) {
+void choice(const int &c, DynArray &arr) {
     clearScreen();
 
     switch (c) {
-	case 0:
-	{
-	    if (list.isEmpty())
-		std::cout << "List is empty\n";
-	    else {
-		ListStreams::Iterator it = list.getHead()->getNextBySurname();
-		std::cout << "List sorted by surname\n";
-		while (it->getNextBySurname()) {
-		    std::cout << it->getSurname() << " ";
-		    std::cout << it->getNumber() << "\n";
-		    it = it->getNextBySurname();
-		}
-		std::cout << "\n\n";
+        case 0: {
+            int size = arr.getLength();
+            if (size < 1)
+                std::cout << "Array is empty\n\n";
+            else {
+                for (int i = 0; i < size; ++i)
+                    std::cout << arr.getValue(i) << " ";
+                std::cout << "\n\n";
+            }
+            break;
+        }
 
-		it = list.getHead()->getNextByNumber();
-		std::cout << "List sorted by number\n";
-		while (it->getNextByNumber()) {
-		    std::cout << it->getNumber() << " ";
-		    std::cout << it->getSurname() << "\n";
-		    it = it->getNextByNumber();
-		}
-		std::cout << "\n\n";
-	    }
-	    break;
-	}
+        case 1: {
+            if(arr.getLength() != 0)
+                arr.clear();
 
-	case 1:
-	{
-	    string name;
-	    unsigned int number;
-	    std::cout << "Enter surname and number: ";
-	    std::cin >> name;
-	    std::cin >> number;
-	    list.push(name, number);
-	    std::cout << "Node pushed\n";
-	    break;
-	}
+            int size, value;
+            std::cout << "Enter array size: ";
+            std::cin >> size;
+            while(size < 1) {
+                std::cout << "Invalid array size, try again: ";
+                std::cin >> size;
+            }
 
-	case 2:
-	{
-	    unsigned int number;
-	    std::cout << "Enter number: ";
-	    std::cin >> number;
+            arr.allocateMemory(size);
+            std::cout << "Enter values: \n";
+            for(int i = 0; i < size; ++i){
+                std::cin >> value;
+                arr.setValue(i, value);
+            }
 
-	    short int result = list.pop(number);
-	    switch (result) {
-		case -1:
-		    std::cout << "List is empty\n";
-		    break;
+            std::cout << "Created array.\n\n";
+            break;
+        }
 
-		case 1:
-		    std::cout << "Node not found\n";
-		    break;
+        case 2: {
+            if(arr.getLength() != 0)
+                arr.clear();
 
-		default:
-		    std::cout << "Node popped\n";
-		    break;
-	    }
-	    break;
-	}
+            int size;
+            std::cout << "Enter array size: ";
+            std::cin >> size;
+            while(size < 1) {
+                std::cout << "Invalid array size, try again: ";
+                std::cin >> size;
+            }
 
-	case 3:
-	{
-	    string name;
-	    std::cout << "Enter surname: ";
-	    std::cin >> name;
+            arr.allocateMemory(size);
+            arr.randomise();
 
-	    short int result = list.pop(name);
-	    switch (result) {
-		case -1:
-		    std::cout << "List is empty\n";
-		    break;
+            std::cout << "Created random array.\n\n";
+            break;
+        }
 
-		case 1:
-		    std::cout << "Node not found\n";
-		    break;
+        case 3: {
+            if(!arr.getLength())
+                std::cout << "Array is empty.\n\n";
+            else{
+                arr.insertionSort();
+                std::cout << "Sorted array.\n\n";
+            }
+            break;
+        }
 
-		default:
-		    std::cout << "Node popped\n";
-		    break;
-	    }
-	    break;
-	}
+        case 4: {
+            if(!arr.getLength())
+                std::cout << "Array is empty.\n\n";
+            else{
+                arr.shellSort();
+                std::cout << "Sorted array.\n\n";
+            }
+            break;
+        }
 
-	case 4:
-	{
-	    string name;
-	    std::cout << "Enter surname: ";
-	    std::cin >> name;
-	    unsigned int result = list.getNumber(name);
-	    switch (result) {
-		case UINT32_MAX - 1:
-		    std::cout << "List is empty\n";
-		    break;
-
-		case UINT32_MAX:
-		    std::cout << "Node not found\n";
-		    break;
-
-		default:
-		    std::cout << name << "'s number is " << result << std::endl;
-		    break;
-	    }
-	    break;
-	}
-
-	case 5:
-	{
-	    unsigned int number;
-	    std::cout << "Enter number: ";
-	    std::cin >> number;
-	    string result = list.getSurname(number);
-	    if (result == "*empty*")
-		std::cout << "List is empty\n";
-	    else if (result == "not found")
-		std::cout << "Node not found\n";
-	    else
-		std::cout << "The owner of number " << number << " is " << result << std::endl;
-	    break;
-	}
-
-	case 6:
-	{
-	    list.clear();
-	    std::cout << "List cleared\n";
-	    break;
-	}
-
-	case 7:
-	{
-	    if (list.isEmpty())
-		std::cout << "List is empty\n";
-	    else
-		std::cout << "List is not empty\n";
-	    break;
-	}
+        default:
+            std::cout << "Invalid input, try again.\n\n";
+            break;
     }
 }
 
-void menu(ListStreams &list) {
+void menu(DynArray &arr) {
     short int c;
     while (true) {
-	std::cout << "Options:\n\n";
-	std::cout << "0. Print list\n";
-	std::cout << "1. Push node\n";
-	std::cout << "2. Find and pop node by number\n";
-	std::cout << "3. Find and pop node by surname (first occurrence)\n";
-	std::cout << "4. Find number by surname\n";
-	std::cout << "5. Find surname by number\n";
-	std::cout << "6. Clear list\n";
-	std::cout << "7. Check if empty\n";
+        std::cout << "Options:\n\n";
+        std::cout << "0. Print array\n";
+        std::cout << "1. Enter array\n";
+        std::cout << "2. Create random array\n";
+        std::cout << "3. Sort array using insertion sort\n";
+        std::cout << "4. Sort array using Shell sort\n";
 
-	std::cin >> c;
+        std::cin >> c;
 
-	choice(c, list);
+        choice(c, arr);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
 #include "DynArray.h"
+#include "Menu.h"
 
 int main() {
-	DynArray a(6);
-//	int in;
+    DynArray arr;
+    menu(arr);
 
-//	for (int i = 0; i < 6; ++i) {
-//		std::cin >> in;
-//		a.setValue(i, in);
-//	}
-	a.randomise();
-    for (int i = 0; i < 6; ++i)
-        std::cout << a.getValue(i) << " ";
-    std::cout << std::endl;
-	a.shellSort();
-	for (int i = 0; i < 6; ++i)
-		std::cout << a.getValue(i) << " ";
-
+//	DynArray a(6);
+////	int in;
+//
+////	for (int i = 0; i < 6; ++i) {
+////		std::cin >> in;
+////		a.setValue(i, in);
+////	}
+//	a.randomise();
+//    for (int i = 0; i < 6; ++i)
+//        std::cout << a.getValue(i) << " ";
+//    std::cout << std::endl;
+//	a.shellSort();
+//	for (int i = 0; i < 6; ++i)
+//		std::cout << a.getValue(i) << " ";
+//
 	return 0;
 }
