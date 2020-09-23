e698854fcbab045920a883bc2c31f4f9f5c9fe0a
Daniel White <xt1zer@outlook.com>
2020-09-23 11:48:16 +0300

    Testing array random generation

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 
 int main() {
 	DynArray a(6);
-	int in;
+//	int in;
 
-	for (int i = 0; i < 6; ++i) {
-		std::cin >> in;
-		a.setValue(i, in);
-	}
+//	for (int i = 0; i < 6; ++i) {
+//		std::cin >> in;
+//		a.setValue(i, in);
+//	}
+	a.randomise();
+    for (int i = 0; i < 6; ++i)
+        std::cout << a.getValue(i) << " ";
+    std::cout << std::endl;
 	a.shellSort();
 	for (int i = 0; i < 6; ++i)
 		std::cout << a.getValue(i) << " ";
