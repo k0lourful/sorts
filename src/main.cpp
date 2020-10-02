ae15be3f06449076e83ca1dcb214d57a94f03392
Daniel White <xt1zer@outlook.com>
2020-10-03 00:51:57 +0300

    Remove unneeded code

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,5 @@ int main() {
     DynArray arr;
     menu(arr);
 
-//	DynArray a(6);
-////	int in;
-//
-////	for (int i = 0; i < 6; ++i) {
-////		std::cin >> in;
-////		a.setValue(i, in);
-////	}
-//	a.randomise();
-//    for (int i = 0; i < 6; ++i)
-//        std::cout << a.getValue(i) << " ";
-//    std::cout << std::endl;
-//	a.shellSort();
-//	for (int i = 0; i < 6; ++i)
-//		std::cout << a.getValue(i) << " ";
-//
-	return 0;
+    return 0;
 }
