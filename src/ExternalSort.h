179ddd60a26b326542b10246c32830c816ae78d3
Daniel White <xt1zer@outlook.com>
2020-10-25 22:02:09 +0300

    Remove separator character, add checking if file is empty, compute loops of sorting

--- src/ExternalSort.h
+++ src/ExternalSort.h
@@ -1,49 +1,43 @@
 #pragma once
+
 #include <fstream>
 
-//определение конца блока
-bool endRange(std::fstream& f) {
-    int tmp;
-    tmp = f.get();
-    tmp = f.get();
-    if (tmp != '\'') f.seekg(-2);
-    else f.seekg(1);
-    return tmp == '\'';
+bool is_empty(std::fstream &f) {
+    return f.peek() == std::fstream::traits_type::eof();
 }
 
-void naturalSort(const char *fileName) {
+int naturalSort(const char *fileName) {
     int s1, s2, a1, a2, mark;
-    std::fstream f(fileName, std::ios::in), f1("nmsort1", std::ios::out),
-            f2("nmsort2", std::ios::out);
+    std::fstream f, f1, f2;
     s1 = s2 = 1;
+    int k = 0;
     while (s1 > 0 && s2 > 0) {
+        f.open(fileName, std::ios::in);
+        if (!f)
+            return -1; // file not found
+
+        f1.open("nmsort1", std::ios::out);
+        f2.open("nmsort2", std::ios::out);
         mark = 1;
         s1 = 0;
         s2 = 0;
         f >> a1;
         if (!f.eof()) {
             f1 << a1 << " ";
-        }
-        if (!f.eof())
             f >> a2;
+        }
         while (!f.eof()) {
             if (a2 < a1) {
                 switch (mark) {
                     case 1: {
-                        f1 << "\' ";
                         mark = 2;
                         s1++;
                         break;
                     }
 
                     case 2: {
-                        f2 << "\' ";
                         mark = 1;
                         s2++;
-                        break;
-                    }
-
-                    default: {
                         break;
                     }
                 }
@@ -59,12 +53,6 @@ void naturalSort(const char *fileName) {
             a1 = a2;
             f >> a2;
         }
-        if (s2 > 0 && mark == 2) {
-            f2 << "\'";
-        }
-        if (s1 > 0 && mark == 1) {
-            f1 << "\'";
-        }
 
         f2.close();
         f1.close();
@@ -73,50 +61,63 @@ void naturalSort(const char *fileName) {
         f.open(fileName, std::ios::out);
         f1.open("nmsort1", std::ios::in);
         f2.open("nmsort2", std::ios::in);
-        if (!f1.eof())
+
+        if (!is_empty(f1) && !is_empty(f2)) ++k;
+        if (!is_empty(f1))
             f1 >> a1;
-        if (!f2.eof())
+        if (!is_empty(f2))
             f2 >> a2;
+
+        int a;
         bool file1, file2;
         while (!f1.eof() && !f2.eof()) {
             file1 = file2 = false;
             while (!file1 && !file2) {
                 if (a1 <= a2) {
                     f << a1 << " ";
-                    file1 = endRange(f1);
-                    f1 >> a1;
+                    f1 >> a;
+                    file1 = (f1.eof() || a < a1);
+                    a1 = a;
                 } else {
                     f << a2 << " ";
-                    file2 = endRange(f2);
-                    f2 >> a2;
+                    f2 >> a;
+                    file2 = (f2.eof() || a < a2);
+                    a2 = a;
                 }
             }
             while (!file1) {
                 f << a1 << " ";
-                file1 = endRange(f1);
-                f1 >> a1;
+                f1 >> a;
+                file1 = (f1.eof() || a < a1);
+                a1 = a;
             }
             while (!file2) {
                 f << a2 << " ";
-                file2 = endRange(f2);
-                f2 >> a2;
+                f2 >> a;
+                file2 = (f2.eof() || a < a2);
+                a2 = a;
             }
         }
         file1 = file2 = false;
         while (!file1 && !f1.eof()) {
             f << a1 << " ";
-            file1 = endRange(f1);
-            f1 >> a1;
+            f1 >> a;
+            file1 = (f1.eof() || a < a1);
+            a1 = a;
         }
         while (!file2 && !f2.eof()) {
             f << a2 << " ";
-            file2 = endRange(f2);
-            f2 >> a2;
+            f2 >> a;
+            file2 = (f2.eof() || a < a2);
+            a2 = a;
         }
+
         f2.close();
         f1.close();
         f.close();
     }
     remove("nmsort1");
     remove("nmsort2");
+
+    return k;
 }
